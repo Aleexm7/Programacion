65764d2ed9673d8ce24a6232397063af85cc7762
Alejandro <alejandromunozdominguez@gmail.com>
2023-05-17 16:59:00 +0200

    Nueva actualizacion ejercicio3 strings

--- ProyectoJava/src/main/java/ejercicios/strings/Ejercicio3.java
+++ ProyectoJava/src/main/java/ejercicios/strings/Ejercicio3.java
@@ -8,6 +8,9 @@ public class Ejercicio3 {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Ingrese una cadena: ");
+		
+		try {
+			
 		String cad = sc.nextLine();
 
 		System.out.println("Ingrese un caracter: ");
@@ -17,14 +20,21 @@ public class Ejercicio3 {
 		for (int i = 0; i < cad.length(); i++) {
 			if (cad.charAt(i) == caracter) {
 				contador++;
+			}else if(Character.isDigit(caracter) || Character.isWhitespace(caracter)) {
+				throw new IllegalArgumentException("No se permiten numeros o espacios en blancos");
 			}
 
 		}
 
 		System.out.println(
 				"El caracter " + caracter + " " + " se repite " + " " + contador + " veces en la cadena " + " " + cad);
+		}catch(IllegalArgumentException e) {
+			System.out.println("Error --> " + e.getMessage());
+		}finally {
+			sc.close();
+		}
 		
-		sc.close();
+		
 	}
 
 
